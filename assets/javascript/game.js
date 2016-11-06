b1b39271f2af5d47c645ab06b185f9d2cf5244f3
Tasha Casagni <Tasha@Tashas-MacBook-Pro.local>
2016-11-05 23:21:34 -0500

    built out if player looses game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var opponentHP= 100;
 var roundCount =0;
-var pokemon = {"eevee":{ "attack" : 100 , "counter" : 100, "HP" : 100},
-				"pikachu":{ "attack" : 100 , "counter" : 100, "HP" : 100},
-				"charmander":{ "attack" : 100 , "counter" : 100, "HP" : 100},
-				"bulbasaur":{ "attack" : 100 , "counter" : 100, "HP" : 100}
+var pokemon = {"eevee":{ "attack" : 100 , "counter" : 5, "HP" : 500},
+				"pikachu":{ "attack" : 10 , "counter" : 5, "HP" : 100},
+				"charmander":{ "attack" : 10 , "counter" : 5, "HP" : 100},
+				"bulbasaur":{ "attack" : 10 , "counter" : 5, "HP" : 100}
 				};
 var endGame ="false";
 var characterChosen =false;
@@ -15,6 +15,7 @@ var userCounterPower;
 var enemyAttack;
 var enemyHP;
 var enemyCounterPower;
+var userBaseAttack;
 //sets up the character selection process
  
 
@@ -25,33 +26,36 @@ $(".gameCharacter").click(function(){
 		$(this).addClass("mainCharacter")
 		$("#pokemonHolder").children().addClass("enemy");
 		$(".sectionHeader").html("<h2> Available Opponents </h2>");
-		$(".verses").html('<h2> V.S. </h2> <button class="btn btn-primary" id= "attack">Attack!</button><button class="btn btn-primary" id="restart"> Start all Over!</button>');
+		
 		//styling for main character
 		$(".mainCharacter").css("background-color","white");
 		//styling for enemies 
 		$(".enemy").css("background-color","red");
 		//grab poke-stats for battle and assign to user(attack,HP,counter)
 		var pokeValue = $(this).attr("id");
+		userBaseAttack = pokemon[pokeValue].attack;
 		userAttack = pokemon[pokeValue].attack;
-		userHP = pokemon[pokeValue].counter;
-		userCounterPower = pokemon[pokeValue].HP;
+		userHP = pokemon[pokeValue].HP;
+		userCounterPower = pokemon[pokeValue].counter;
 		characterChosen = true;
-		console.log(userAttack);
-		console.log(userHP);
-		console.log(userCounterPower);
+		// console.log(userAttack);
+		// console.log(userHP);
+		// console.log(userCounterPower);
 		
 
 
 		//picks out stagged enemy && makes sure player cant trigger onclick after both characters are set
 	 } else if (characterChosen === true && opponentStaged === false) {
-		console.log(this);
+		$(".enemyArena").html(" ");
 		$(this).appendTo(".enemyArena");
 		$(this).addClass("mainEnemy");
 		var pokeValue = $(this).attr("id");
+		$(".verses").html('<h2> V.S. </h2> <button class="btn btn-primary" id= "attack">Attack!</button>');
 		enemyAttack = pokemon[pokeValue].attack;
-		enemyHP = pokemon[pokeValue].counter;
-		enemyCounterPower = pokemon[pokeValue].HP;
+		enemyHP = pokemon[pokeValue].HP;
+		enemyCounterPower = pokemon[pokeValue].counter;
 		opponentStaged = true;
+		$(".update").html("<div>" + "Your Attack Power is " + userAttack+"</div>" + "<div>" + "Your HP is " + userHP+"</div>" + "<div>" + "Your Enemies HP is " + enemyHP +"</div>" );
 		console.log(enemyAttack);
 		console.log(enemyHP);
 		console.log(enemyCounterPower);	
@@ -61,12 +65,65 @@ $(".gameCharacter").click(function(){
 
 
 
+//when the user hits the attack button
+$(".verses").on("click", "#attack",function(){	
+	//if statement stops user from clicking button with no opponent
+	if (opponentStaged === false){
+		return;
+	}
 
-// $("#attack").click(function(){
-// //makes your HP grow everyround  
-// userAttack += userAttack
+	else {
 
-// });
+		
+		//makes your attack grow everyround  
+		userAttack += userBaseAttack
+		console.log("user attack power" + userAttack);
+
+		//enemy new stats 
+		enemyHP -= (userAttack - enemyCounterPower);
+		console.log("enemy new hp" + enemyHP)
+
+
+		//main characters health depreciation upon attack
+		userHP -=(enemyAttack - userCounterPower);
+		console.log("your new hp" + userHP);
+
+		//updates update section with new stats
+		$(".update").html("<div>" + "Your Attack Power is " + userAttack+"</div>" + "<div>" + "Your HP is " + userHP+"</div>" + "<div>" + "Your Enemies HP is " + enemyHP +"</div>" );
+
+		// updates progress bar with new health 
+		$(".update").html();
+
+		//when enemy faints
+		if(enemyHP <= 0){
+			console.log("enemy fainted");
+			opponentStaged = false;
+			$(".mainEnemy").remove();
+			$(".enemyArena").html("Please Pick A New Opponent!");
+		}
+		// when main character faints 
+		if (userHP <=0){
+			console.log("you fainted");
+			$("body").html('<div> <p> Game Over! </p> </div>  <div> <button class="btn btn-primary" id="restart"> Start all Over!</button> </div>');
+			$("body").css({"color": "white", "background-color": "black", "background-image" : "url(assets/images/pika-faint.jpg)",
+			"background-size" : "300px 300px" , "background-repeat": "no-repeat", "background-attachment": "fixed",
+   			 "background-position": "50% 50%"});
+		}
+	}
+
+});
+
+
+// when they click on the start over button
+$("body").on("click", "#restart",function(){	
+	console.log("restart butotn clicked");
+	location.reload();
+
+  
+
+});
+
+
 
 //oponent attack 
 
